635dbb41480c2e8f07eab275485afb397f3b923e
Maxim Markitantov <m.markitantov@yandex.ru>
2025-06-09 17:11:23 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# TRIFONES page repository
+The official repository for Multi-Lingual Approach for Multi-Modal Emotion and Sentiment Recognition Based on Triple Fusion (TRIFONES) page
+
+## Abstract
+> Affective states recognition is a challenging task that requires a large amount of input data, such as audio, video, and text. Current multi-modal approaches are often single-task and corpus-specific, resulting in overfitting, poor generalization across corpora, and reduced real-world performance. In this work, we address these limitations by: (1) multi-lingual training on corpora that include Russian (RAMAS) and English (MELD, CMU-MOSEI) speech; (2) multi-task learning for joint emotion and sentiment recognition; and (3) a novel Triple Fusion strategy that employs cross-modal integration at both hierarchical unimodal and fused multi-modal feature levels, enhancing intra- and inter-modal relationships of different affective states and modalities. Additionally, to optimize performance of the approach proposed, we compare temporal encoders (Transformer-based, Mamba, xLSTM) and fusion strategies (double and triple fusion strategies with and without a label encoder) to comprehensively understand their capabilities and limitations. On the Test subset of the CMU-MOSEI corpus, the proposed approach showed mean weighted F1-score (mWF) of 88.6%, and weighted F1-score (WF) of 84.8% for emotion and sentiment recognition, respectively. On the Test subset of the MELD corpus, the proposed approach showed WF of 49.6% and WF of 60.0%, respectively. On the Test subset of the RAMAS corpus, the proposed approach showed WF of 71.8% and WF of 90.0%, respectively. We compare the performance of the approach proposed with that of the SOTA ones.
+
+## Acknowledgments
+
+Parts of this project page were adopted from the [Nerfies](https://nerfies.github.io/) page.
+
+## Website License
+
+<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
---
+++ static/js/comp-slider.js
@@ -0,0 +1,83 @@
+const btn = document.querySelector('#btt-button');
+const bttArr = document.querySelector('#btt-arr');
+const sliderItems = document.querySelectorAll('.slider__item');
+const sliderLine = document.querySelector('.slider__line');
+const sliderDots = document.querySelectorAll('.slider__dot');
+const sliderBtnNext = document.querySelector('.slider__btn-next');
+const sliderBtnPrev = document.querySelector('.slider__btn-prev');
+
+// Появление стрелочки навверх в середине страницы
+window.addEventListener('scroll', function () {
+    if (window.scrollY > 100) {
+        btn.classList.add('show');
+    } else {
+        btn.classList.remove('show');
+    }
+});
+
+btn.addEventListener('mouseover', function (e) {
+    bttArr.classList.add("btt-hover");
+});
+
+btn.addEventListener('mouseleave', function (e) {
+    e.preventDefault();
+    bttArr.classList.remove('btt-hover');
+});
+
+btn.addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+let sliderCount = 0;
+let sliderWidth;
+
+// адаптивность
+window.addEventListener('resize', showSlide);
+
+function showSlide() {
+    sliderWidth = document.querySelector('.slider').offsetWidth;
+    sliderLine.style.width = sliderWidth * sliderItems.length + 'px';
+    sliderItems.forEach(item => item.style.width = sliderWidth + 'px');
+
+    rollSlider();
+}
+
+showSlide();
+
+function nextSlide() {
+    sliderCount++;
+    if (sliderCount >= sliderItems.length) sliderCount = 0;
+    rollSlider();
+    thisSlide(sliderCount);
+}
+
+function prevSlide() {
+    sliderCount--;
+    if (sliderCount < 0) sliderCount = sliderItems.length - 1;
+
+    rollSlider();
+    thisSlide(sliderCount);
+}
+
+sliderBtnNext.addEventListener('click', nextSlide);
+sliderBtnPrev.addEventListener('click', prevSlide);
+
+function rollSlider() {
+    sliderLine.style.transform = `translateX(${-sliderCount * sliderWidth}px)`;
+}
+
+function thisSlide(index) {
+    sliderDots.forEach(item => item.classList.remove('active-dot'));
+    sliderDots[index].classList.add('active-dot')
+}
+
+// клик на dots
+sliderDots.forEach((dot,index) => {
+    dot.addEventListener('click', () => {
+        sliderCount = index;
+
+        rollSlider();
+        thisSlide(sliderCount);
+    });
+})
